4b147a592676419ee8826e86fff9ed25ef5b2598
gschecho <gschecho@gmail.com>
2022-02-14 22:52:14 +0100

    Nuevas funcionalidades

--- js/selection.js
+++ js/selection.js
@@ -1,4 +1,20 @@
 const selection_input_size = document.querySelector('.bloque__sel');
+const number_input= document.querySelector('#input__number')
+const increment_number = document.querySelector('#add');
+const decrement_number = document.querySelector('#subtract')
 
 
-console.log(selection_input_size)
+
+//incrementar y quitar bizcohos
+
+increment_number.addEventListener('click', function(){
+    number_input.value++
+}) 
+decrement_number.addEventListener('click', function(){
+    number_input.value--
+}) 
+
+// check selección
+
+console.log(selection_input_size.childNodes);
+
